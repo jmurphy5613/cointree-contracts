4859fb75d60d093eaa75eba666dcf9315d42b1eb
jmurphy5613 <jmurphy5613@gmail.com>
2022-09-03 14:32:26 -0400

    lol

--- test/Lock.js
+++ test/Lock.js
@@ -1,8 +1,12 @@
 
 const main = async () => {
-  const contract = await hre.ethers.getContractFactory("Vault");
-  const deployedContract = await contract.deploy();
-  console.log("contract deployed to: ", deployedContract.address);
+  const vault = await hre.ethers.getContractFactory("Vault");
+  const deployedVault = await vault.deploy();
+  console.log("vault deployed to: ", deployedVault.address);
+
+  const NFT = await hre.ethers.getContractFactory("CoinTreeNFT");
+  const deployedNFT = await NFT.deploy();
+  console.log("NFT deployed to: ", deployedNFT.address);
 }
 
 main()
